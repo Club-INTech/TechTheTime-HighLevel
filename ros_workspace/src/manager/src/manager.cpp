e90b037f76e8bfc3cddf50b4c891c8a41ad25470
Occalepsus <juju.joly@free.fr>
2022-02-01 16:14:28 +0100

    Subscriber try

--- ros_workspace/src/manager/src/manager.cpp
+++ ros_workspace/src/manager/src/manager.cpp
@@ -7,7 +7,7 @@
 #include <stdexcept>
 #include "rclcpp/rclcpp.hpp"
 #include "action_msg_srv/srv/order.hpp"
-#include "image_msg_srv/srv/imageRequest.hpp"
+#include "action_msg_srv/srv/image.hpp"
 
 
 int main(int argc, char **argv) {
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
     auto commClient = new ClientNode<action_msg_srv::srv::Order, action_msg_srv::srv::Order::Request, std::string, int64_t, int64_t, int64_t, int64_t>("action_client");
     commClient->wait_for_connection();
 
-    auto imgClient = new ClientNode<image_msg_srv::srv::ImageRequest, image_msg_srv::srv::ImageRequest::Request, std::string, int64_t>("image_client");
+    auto imgClient = new ClientNode<action_msg_srv::srv::Image, image_msg_srv::srv::Image::Request, std::string>("image_client");
     imgClient->wait_for_connection();
 
     rclcpp::shutdown();
---
+++ ros_workspace/src/manager/src/subscriber/Subscriber.hpp
@@ -0,0 +1,21 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+template<class T>
+class Subscriber : public rclcpp::Node
+{
+public:
+    Subscriber()
+        : Node("minimal_subscriber")
+    {
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
+
+private:
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    {
+        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
