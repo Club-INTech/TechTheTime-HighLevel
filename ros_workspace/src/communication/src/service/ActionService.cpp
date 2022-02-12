779abab4ef680ad62fef299b129d51e610f1e9cb
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-02-12 23:13:01 +0100

    Move binding example in action service

--- ros_workspace/src/communication/src/service/ActionService.cpp
+++ ros_workspace/src/communication/src/service/ActionService.cpp
@@ -3,16 +3,26 @@
 #include "action_msg_srv/srv/order.hpp"
 
 #include <memory>
+#include "order_binder.hpp"
+#include "order_codes.hpp"
 
-ActionService::ActionService(const std::string& service_name) : Node(service_name), service_name(service_name),
+#include <chrono>
+#include <thread>
+
+ActionService::ActionService(const std::string& service_name) : Node(service_name), order_binder(), service_name(service_name),
     service(this->create_service<action_msg_srv::srv::Order>(service_name, [&](
-        const std::shared_ptr<action_msg_srv::srv::Order::Request> req,
-        std::shared_ptr<action_msg_srv::srv::Order::Response> res){ this->treat_orders(req, res);})) {}
+        const shared_request_T req, shared_response_T res){ this->treat_orders(req, res);})) {
+
+                order_binder.bind_order(OrderCodes::MOVE, [](shared_request_T req, shared_response_T res){
+                        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Moving for the distance: %d\n",req->distance);
+                        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+                        res->success = true;
+                        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%d]", (bool)res->success);                
+                });
+
+        }
 
 void ActionService::treat_orders(const std::shared_ptr<action_msg_srv::srv::Order::Request> req,
         std::shared_ptr<action_msg_srv::srv::Order::Response> res) {
-        res->success = true;
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request: %d\n",
-                req->x);
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%d]", (bool)res->success);
+        this->order_binder.execute_order(req->order_code, req, res);
 }
