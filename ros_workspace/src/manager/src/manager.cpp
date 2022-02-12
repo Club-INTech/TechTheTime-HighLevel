e77c2687abc69a174aaa50cf49c67fb139a6d1ed
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-02-12 23:13:31 +0100

    Example of manager <-> communication com

--- ros_workspace/src/manager/src/manager.cpp
+++ ros_workspace/src/manager/src/manager.cpp
@@ -7,6 +7,7 @@
 #include <stdexcept>
 #include "rclcpp/rclcpp.hpp"
 #include "action_msg_srv/srv/order.hpp"
+#include "order_codes.hpp"
 
 // #include "subscriber/testSubscriber.hpp"
 
@@ -16,14 +17,13 @@ int main(int argc, char **argv) {
 
     rclcpp::init(argc, argv);
     auto commClient = std::make_shared<ClientT<action_msg_srv::srv::Order, action_msg_srv::srv::Order::Request,
-                std::string, int64_t, int64_t, int64_t, int64_t>>("action");
+                int64_t, int64_t, int64_t, int64_t>>("action");
     //On dit à ros où envoyer la réponse
     commClient->set_shared(commClient);
     commClient->wait_for_connection();
 
-    commClient->send("get_left_ticks", 0, 0, 0, 0);
-    commClient->send("get_right_ticks", 0, 0, 0, 0);
-    commClient->send("move", 0, 0, 0, 0);
+    commClient->send(OrderCodes::MOVE, 25, 0, 0);
+    
 
     rclcpp::shutdown();
 
