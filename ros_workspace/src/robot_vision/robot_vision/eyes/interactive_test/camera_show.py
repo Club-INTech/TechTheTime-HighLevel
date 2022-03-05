8dbb9c0f61d8465da725f384ec3b361fcc264990
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-03-05 20:21:10 +0100

    Save the code in test.py to reuse it later. Finish interactive tests to show camera video flow and to detect aruco tags

--- ros_workspace/src/robot_vision/robot_vision/eyes/interactive_test/camera_show.py
+++ ros_workspace/src/robot_vision/robot_vision/eyes/interactive_test/camera_show.py
@@ -2,7 +2,6 @@ import robot_vision.eyes.eyes as eyes
 import cv2 as cv
 from robot_vision.eyes.exception.eyes_exception import EyesException
 
-
 if __name__ == "__main__":
     c = eyes.Eyes(2, **{
         'color_transform': cv.COLOR_RGB2GRAY,
@@ -10,8 +9,7 @@ if __name__ == "__main__":
     })
     while True:
         try:
-            _, res = c.look("detect")
-            print(res)
+            _, _ = c.look("show")
         except EyesException as e:
             print(e)
             continue
