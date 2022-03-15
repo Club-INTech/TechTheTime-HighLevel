ad0187326738de30c1e8f68974d81420ebbd4b01
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-03-15 19:40:28 +0100

    action_msg_srv_shared doc

--- ros_workspace/src/srvs_msgs/action_msg_srv_shared/order_binder.hpp
+++ ros_workspace/src/srvs_msgs/action_msg_srv_shared/order_binder.hpp
@@ -5,19 +5,50 @@
 #include <functional>
 #include <cstdint>
 
+/**
+ * OrderBinder is used by ActionService in order to execute callback attached to an order code.
+ * You can bind any of OrderCodes(order_codes.hpp) to any function with the OrderBinder::order_functor_T signature and then execute
+ * it by providing an <b>order_code</b> and parameters.
+ * 
+ * @tparam Treq The type of request for the order
+ * @tparam Tres The type of response of an order 
+*/ 
 template<typename Treq, typename Tres>
 struct OrderBinder {
 
-    using wrapped_func_T = typename std::function<void(Treq, Tres)>;
+    /**
+     * An alias to the function type that can be associated to one of the OrderCodes(order_codes.hpp) 
+    */ 
+    using order_functor_T = typename std::function<void(Treq, Tres)>;
 
-    std::unordered_map<int64_t, wrapped_func_T> orders;
+    /**
+     * A map that associate an <b>order_code</b> to the callback
+    */ 
+    std::unordered_map<int64_t, order_functor_T> orders;
 
+    /**
+     * Default constructor, calls a default constructor of OrderBinder::orders 
+    */ 
     OrderBinder() : orders() {}
 
-    constexpr void bind_order(int64_t order_code, wrapped_func_T order) {
+    /**
+     * Associates an order(an OrderBinder::order_functor_T) to an order_code
+     * @param order_code A code of an order (type is int64_t to allow a direct execution of an associated order 
+     * after receiving it from the ClientT)
+     * @param order A functor associated to the <b>order_code</b> 
+    */ 
+    constexpr void bind_order(int64_t order_code, order_functor_T order) {
         orders.insert({order_code, order});
     }
 
+    /**
+     * Executes an order(an OrderBinder::order_functor_T) associated to an <b>order_code</b>
+     * 
+     * @param order_code A code of an order (type is int64_t to allow a direct execution of an associated order 
+     * after receiving it from the ClientT)
+     * @param req A received request, that must be treated
+     * @param res A response that must be composed and sended back
+    */ 
     void execute_order(int64_t order_code, Treq req, Tres res) {
         (orders.find(order_code)->second)(req, res);
     }
--- ros_workspace/src/srvs_msgs/action_msg_srv_shared/order_codes.hpp
+++ ros_workspace/src/srvs_msgs/action_msg_srv_shared/order_codes.hpp
@@ -1,14 +1,29 @@
 #ifndef ORDER_CODES_HPP
 #define ORDER_CODES_HPP
 
+/**
+ * OrderCodes are used to distinguish orders and send them from manager to microcontroller_proxy. 
+*/ 
 enum OrderCodes {
     MOVE,
     ROTATE
 };
 
+/**
+ * MotionStatusCodes are used to determine the status of the robot after movement 
+*/ 
 enum MotionStatusCodes {
+    /**
+     * MOTION_TIMEOUT indicates that the requested motion has not been done in required time interval  
+    */ 
     MOTION_TIMEOUT,
+    /**
+     * NOT_COMPLETE indicates that the robot stopped without completing the movement 
+    */ 
     NOT_COMPLETE,
+    /**
+     * COMPLETE indicates that the robot completed movement and stopped 
+    */ 
     COMPLETE
 };
 
