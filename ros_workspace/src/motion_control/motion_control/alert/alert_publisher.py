b06151f5692a37c83a5ff148cedce9b78f1f9331
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-04-04 22:44:37 +0200

    Motion control fix + alert publisher

--- ros_workspace/src/motion_control/motion_control/alert/alert_publisher.py
+++ ros_workspace/src/motion_control/motion_control/alert/alert_publisher.py
@@ -1,1 +1,26 @@
-import numpy as np
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Bool
+
+
+class AlertPublisher(Node):
+
+    def __init__(self):
+        super().__init__('alert_publisher')
+        self.publisher_ = self.create_publisher(Bool, 'alert', 10)
+        self.__alert = False
+    
+    def alert(self):
+        self.__alert = True
+        self.__publish_msg()
+
+    def stop_alert(self):
+        self.__alert = False
+        self.__publish_msg()
+
+    def __publish_msg(self):
+        msg = Bool()
+        msg.data = self.__alert
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: ' +  str(msg.data))
--- ros_workspace/src/motion_control/motion_control/motion_control.py
+++ ros_workspace/src/motion_control/motion_control/motion_control.py
@@ -1,2 +1,16 @@
-def start():
-    pass
+import rclpy
+from alert.alert_publisher import AlertPublisher
+
+if __name__ == "__main__":
+    rclpy.init(args=None)
+    alert_publisher = AlertPublisher()
+    while(True):
+        alert = bool(input())
+        if alert:
+            alert_publisher.alert()
+        else:
+            alert_publisher.stop_alert()
+
+    alert_publisher.destroy_node()
+    rclpy.shutdown()
+
