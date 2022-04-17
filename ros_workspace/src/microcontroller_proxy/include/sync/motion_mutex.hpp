db29cc6d406889b591c8e416f5fe3ecbbdee8b98
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-04-17 20:48:26 +0200

    Add motion status control mutex in sync

--- ros_workspace/src/microcontroller_proxy/include/sync/motion_mutex.hpp
+++ ros_workspace/src/microcontroller_proxy/include/sync/motion_mutex.hpp
@@ -10,18 +10,21 @@
 namespace motion_mutex {
 
     extern std::mutex order_mutex;
+    extern std::mutex status_mutex;
     extern AlertMutex alert_mutex;
 
     template<auto Ftor, typename... Args>
-    void sync_call(bool order_access,
+    void sync_call(bool order_access, bool status_access,
     bool alert_access, Args&&... args) {
 
         if(order_access) order_mutex.lock();
+        if(status_access) status_mutex.lock();
         if(alert_access) alert_mutex.lock();
 
         std::invoke(Ftor, std::forward<Args>(args)...);
 
         if(order_access) order_mutex.unlock();
+        if(status_access) status_mutex.unlock();
         if(alert_access) alert_mutex.unlock();
     }
 };
