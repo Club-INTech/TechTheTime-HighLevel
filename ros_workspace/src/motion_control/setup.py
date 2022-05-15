a5edfb09f130605d1fbabf8a1a517b93ef4ac90c
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-05-15 16:01:11 +0200

    Fix scripts by renaming moveABS to moveREL. Fix executable name of motion_control

--- ros_workspace/src/motion_control/setup.py
+++ ros_workspace/src/motion_control/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'listener = motion_control.motion_control:main',
+            'motion_control = motion_control.motion_control:main',
         ],
     },
 )
