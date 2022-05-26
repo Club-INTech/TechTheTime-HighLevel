c39b3b961332cc9edf15784e683b7717f689ee36
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-05-26 20:03:28 +0200

    replace ros multithreading by homemade

--- ros_workspace/src/motion_control/motion_control/motion_control.py
+++ ros_workspace/src/motion_control/motion_control/motion_control.py
@@ -6,11 +6,13 @@ import yaml
 from motion_control.motion_subscriber import MotionSubscriber
 from motion_control.points_subscriber import PointsSubscriber
 from motion_control.const import DEFAULT_SCAN_PROCESSING_DELAY_NS, DEFAULT_STOP_PRECISION_MM
+import threading
+
+def runner(node):
+    rclpy.spin(node)
 
 
 def main(args=None):
-
-    config_file = sys.argv[1]
 
     team = "yellow"
     robot = "slave"
@@ -18,7 +20,7 @@ def main(args=None):
     delay = DEFAULT_SCAN_PROCESSING_DELAY_NS
     precision = DEFAULT_STOP_PRECISION_MM
 
-    threshold = 50;
+    threshold = 50
 
     if len(sys.argv) >= 2:
         with open(sys.argv[1], 'r') as config:
@@ -36,14 +38,18 @@ def main(args=None):
 
 
     rclpy.init(args=args)
-    executor = rclpy.executors.MultiThreadedExecutor(num_threads=multiprocessing.cpu_count() or 2)
+
     motion_subscriber = MotionSubscriber(team, robot)
     points_subscriber = PointsSubscriber(delay, precision, threshold)
 
-    executor.add_node(motion_subscriber)
-    executor.add_node(points_subscriber)
+    motion_thread = threading.Thread(target=runner, args=(motion_subscriber))
+    points_thread = threading.Thread(target=runner, args=(points_subscriber))
 
-    executor.spin()
+    motion_thread.start()
+    points_thread.start()
+
+    motion_thread.join()
+    points_thread.join()
 
     motion_subscriber.destroy_node()
     points_subscriber.destroy_node()
