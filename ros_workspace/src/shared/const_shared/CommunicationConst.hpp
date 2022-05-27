6d195cfe0f4f7eba3e78a6ce2615b565980da257
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-05-27 10:50:26 +0200

    Quick fix

--- ros_workspace/src/shared/const_shared/CommunicationConst.hpp
+++ ros_workspace/src/shared/const_shared/CommunicationConst.hpp
@@ -13,7 +13,7 @@ constexpr std::chrono::milliseconds READ_FEEDBACK_DELAY = 0ms;
 
 constexpr std::chrono::milliseconds WAITING_PERIOD = 100ms;
 
-constexpr std::chrono::milliseconds JUMPER_WAITING = 1000ms;
+constexpr std::chrono::milliseconds JUMPER_WAITING = 20ms;
 
 constexpr std::chrono::milliseconds ARM_WAITING_PERIOD = 1000ms;
 constexpr std::chrono::milliseconds PUMP_WAITING_PERIOD = 1000ms;
