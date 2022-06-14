c5f05282eb6104d4cdc5662f266171ce255552cd
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2022-06-14 08:29:42 +0200

    Changes for monitor

--- monitor/back/app.py
+++ monitor/back/app.py
@@ -1,9 +1,9 @@
-from requests import request
 from flask import Flask
 import random
 from flask_cors import CORS
 import time
 import math
+from flask import request
 
 app = Flask(__name__)
 CORS(app)
@@ -23,17 +23,51 @@ prev_t = time.time()
 
 @app.route("/data")
 def get_data():
+    global x
+    global prev_x
+    global y
+    global prev_y
+    global angle
+    global speed
+    global order 
+    global status
+    global alert
+    global t
+    global prev_t
     return {"x": x, "y": y, "angle": angle, "speed": speed, "order": order, "status": status, "alert": alert}
 
 @app.route("/pos", methods=['POST', 'GET'])
 def position():
+
     if request.method == 'POST':
-        x = request.args.get('x', '')
-        y = request.args.get('y', '')
-        angle = request.args.get('angle')
+
+        global x
+        global prev_x
+        global y
+        global prev_y
+        global angle
+        global speed
+        global order 
+        global status
+        global alert
+        global t
+        global prev_t
+
+        _x = request.args.get('x')
+        print(_x)
+        _y = request.args.get('y')
+        _angle = request.args.get('angle')
+        x = float(_x)
+        y = float(_y)
+        _angle = float(_angle)
         t = time.time()
         speed = math.sqrt((prev_x - x)**2 + (prev_y - y)**2) / (t - prev_t)
         prev_t = t
         prev_y = y
         prev_x = x
-        alert = request.args.get('alert', '')
+        _alert = request.args.get('alert')
+        alert = _alert
+        print(speed)
+        print(alert)
+
+    return "success"
